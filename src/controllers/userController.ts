95977a51cafb31c89da1f019a8cc17439e79b5ef
GitiCat <39192983+GitiCat@users.noreply.github.com>
2021-06-04 16:54:08 +0300

    temp

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -24,11 +24,45 @@ const createJwt = (id: number, login: string): Promise<string> => {
 }
 
 const UserController = () => {
-    const loginGET = (req: Request, res: Response) => {
+    const loginGETForm = (req: Request, res: Response) => {
         res.render('login', {
             title: 'Авторизация',
             layout: 'authLayout'
         })
+    }
+
+    const getAll = async (req: Request, res: Response) => {
+        const users = await db.Users.findAll()
+        res.status(200).json(users)
+    }
+
+    const getById = async (req: Request, res: Response, next: NextFunction) => {
+        const { id } = req.query
+        if (!id) return next(res.status(400).json({ message: 'You must specify user ID' }))
+
+        const user = await db.Users.findOne({ where: { id: id } })
+        if (!user) return next(res.status(404).json({ message: `User with ${id} is not present in database` }))
+
+        res.status(200).json(user)
+    }
+
+    const deleteById = async (req: Request, res: Response, next: NextFunction) => {
+        const { id } = req.query
+        if (!id) return res.status(400).json({ message: 'You must specify user ID' })
+
+        const result = await db.Users.destroy({ where: { id: id } })
+        res.status(200).json(result)
+    }
+
+    const changeById = async (req: Request, res: Response, next: NextFunction) => {
+        const { id } = req.query
+        if (!id) return next(res.status(400).json({ message: 'You must specify user ID' }))
+
+        const user = await db.Users.findOne({ where: { id: id } })
+        if (!user) return next(res.status(404).json({ message: `User with ${id} is not present in database` }))
+
+        const { login, password, username } = req.body
+        res.status(200).json(user.update({ login: login, password: password, username: username }))
     }
 
     const login = async (req: Request, res: Response, next: NextFunction) => {
@@ -45,21 +79,25 @@ const UserController = () => {
 
     const register = async (req: Request, res: Response, next: NextFunction) => {
         const error: Result<ValidationError> = validationResult(req)
-        if(!error.isEmpty()) return next(res.status(400).json(error.array()[0]))
+        if (!error.isEmpty()) return next(res.status(400).json(error.array()[0]))
 
         const { login, password, username } = req.body
 
         try {
             const hashPassword: string = await bcrypt.hash(password, Number(process.env.JWT_SALT_ROUND))
-            const user = await db.Users.create({login: login, password: hashPassword, username: username})
-            res.status(201).json({user})
-        } catch(error) {
-            return next(res.status(500).json({error: error}))
+            const user = await db.Users.create({ login: login, password: hashPassword, username: username })
+            res.status(201).json({ user })
+        } catch (error) {
+            return next(res.status(500).json({ error: error }))
         }
     }
 
     return {
-        loginGET,
+        loginGETForm,
+        getAll,
+        getById,
+        deleteById,
+        changeById,
         login,
         register
     }
--- src/routers/user/index.ts
+++ src/routers/user/index.ts
@@ -5,6 +5,10 @@ import UserController from '../../controllers/userController'
 export const userRouter: Router = Router()
 const userController = UserController()
 
-userRouter.get('/login', userController.loginGET)
+userRouter.get('/login', userController.loginGETForm)
+userRouter.get('/users', userController.getAll)
+userRouter.get('/users/:id', userController.getById),
+userRouter.put('/users/:id', userController.changeById)
+userRouter.delete('/users/:id', userController.deleteById)
 userRouter.post('/login', userRules.login, userController.login)
 userRouter.post('/register', userRules.register, userController.register)
